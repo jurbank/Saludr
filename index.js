82c4b238929bfc5e20254f7b2747bc93d1844a5b
jurbank <jurbank@fallsdigital.com>
2019-06-11 21:43:20 -0400

    updates

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const apikey = process.env.SALUDR_API_KEY;
-const apisecret = process.env.SALUDR_API_SECRET;
+
 const doteenv = require ('dotenv').config();
 const express = require ('express');
 const app = express();
@@ -13,15 +12,24 @@ const forwardingaddress = "https://boiling-inlet-58072.herokuapp.com/"
 const apiKey = "f0c01221a35fc4164bdb0015ae7f8b41";
 const apiSecret = "d033f7ce079d7b048525d8af3cc7e9db";
 const scopes = "write_products"
+const forwardingAddress = 'https://saludr-2.herokuapp.com'
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/shopify',(req,res) => {
     const shop = req.query.shop;
     if (shop) {
-        console.log('has shop param!')
+        const state = nonce();
+        const redirectUri = forwardingAddress + '/shopify/callback'
+        const installUrl = 'https://' + shop + '/admin/oauth/authorize?client_id=' + apiKey +
+        '&scope=' + scopes +
+        '&state=' + state +
+        '&redirect_uri=' + redirectUri;
+
+        console.log(shop,'log shop')
+        res.cookie('state', state)
+        res.redirect(installUrl)
     } else {
-        console.log('no shop param :(')
         return res.status(400).send('Missing shop parameter. Please add ?shop=your-dev-shop.myshopify.com')
     }
 });
